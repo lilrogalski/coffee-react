000b4fb30919b46f43dab7b4a2d2ea5315926683
Ryan Rogalski <ryan.rogalski@gmail.com>
2016-02-12 13:22:05 -0800

    serve static express

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
